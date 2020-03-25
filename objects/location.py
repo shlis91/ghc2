3224867d4add6bcb9bf60a0df84b75d29b6d69d2
Matan Rak <matanrak@me.com>
2020-03-25 12:30:23 +0200

    Populated the 'distance' method

--- objects/location.py
+++ objects/location.py
@@ -1,10 +1,11 @@
 from typing import NamedTuple
+import math
 
 
 class Location(NamedTuple):
+    """ Describes a location, allows distance calculation """
     x: int
     y: int
 
-    def distance(self, Location):
-        # TODO Actually code this
-        return 1
+    def distance(self, destination: 'Location') -> int:
+        return round(math.sqrt((self.x - destination.x) ^ 2 + (self.y - destination.y) ^ 2))
