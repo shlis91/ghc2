c41d48fdfd3c82e9c45d939029a58bc49b0359df
Matan Rak <matanrak@me.com>
2020-03-25 13:10:12 +0200

    Created super basic path finding

--- objects/location.py
+++ objects/location.py
@@ -9,3 +9,9 @@ class Location(NamedTuple):
 
     def distance(self, destination: 'Location') -> int:
         return round(math.sqrt((self.x - destination.x) ^ 2 + (self.y - destination.y) ^ 2))
+
+    def get_next_location_in_path(self, destination: 'Location') -> 'Location':
+        new_x: int = self.x + min(math.fabs(self.x - destination.x), 1)
+        new_y: int = self.y + min(math.fabs(self.y - destination.y), 1)
+
+        return Location(new_x, new_y)
