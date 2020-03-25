8b0d0ed0353d79ec846a42619f2dee27bdacc77b
Matan Rak <matanrak@me.com>
2020-03-25 12:08:25 +0200

    Populated deliver function

--- objects/drone.py
+++ objects/drone.py
@@ -1,26 +1,32 @@
-from typing import Tuple, Callable, NoReturn
+from typing import Tuple, Callable, NoReturn, NamedTuple
 
 from objects.location import Location
 from objects.package import Package
 
 
-class Drone:
+class ActionTuple(NamedTuple):
+    func: Callable
+    args: tuple
+    kwargs: dict
 
+
+class Drone:
     def __init__(self, drone_id: int, location: Location):
         self.drone_id: int = drone_id
         self.location: Location = location
 
-        self.latest_action: Tuple[Callable, bool] = (None, None)
+        self.current_action: ActionTuple = None
 
-    def _deliver(self, destination: Tuple[int, int], package: Package) -> NoReturn:
+    def _deliver(self, destination: Location, package: Package) -> NoReturn:
         """ Contains the logic for delivery,
         runs each turn while the drone is in delivery mode.
 
         :param destination: The delivery destination
         :param package: The package to be delivered
         """
+        pass
 
-    def deliver(self, destination: Tuple[int, int], package: Package, dry: bool = False) -> int:
+    def deliver(self, destination: Location, package: Package, dry: bool = False) -> int:
         """ Commands the drone to deliver the package to the destination.
 
         :param destination: The delivery destination
@@ -30,7 +36,10 @@ class Drone:
 
         :return: The amount of turns required for the drone to deliver the package.
         """
-        pass
+        if dry is False:
+            self.current_action = ActionTuple(self._deliver, (destination, package), {})
+
+        return self.location.distance(destination)
 
     def turns_left(self) -> int:
         """ Amount of turns until the drone finishes his current action. """
