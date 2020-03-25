d93315696c6e154d71486a9e7d1a14a0d3701884
Matan Rak <matanrak@me.com>
2020-03-25 13:59:41 +0200

    "FiNIsHED"? Drone logic and deleted Product

--- objects/drone.py
+++ objects/drone.py
@@ -1,16 +1,16 @@
-from typing import Callable, NoReturn, NamedTuple
+from typing import Callable, NoReturn, NamedTuple, List
+from functools import wraps
 
 from objects.location import Location
-from objects.product import Product
+from objects.order import Order
+from objects.warehouse import Warehouse
 
 
-class ActionTuple(NamedTuple):
+class Task(NamedTuple):
     func: Callable
     destination: Location
-    package: Product
 
     args: tuple = tuple()
-    kwargs: dict = dict()
 
 
 class Drone:
@@ -18,89 +18,87 @@ class Drone:
         self.drone_id: int = drone_id
         self.location: Location = location
 
-        self.current_action: ActionTuple = None
+        self.task_list: List[Task] = list()
+        self.inventory: List[int] = list()
+
+    @staticmethod
+    def _requires_location(func):
+        """ Runs before a drone action that requires the drone to be in a location.
+        If the drone isn't yet in the location, it will move towards it without running the function.
+        Else the function will run. """
+        @wraps(func)
+        def wrapper(self: Drone, order: Order) -> NoReturn:
+            if self.location != order.location:
+                self.location = self.location.get_next_location_in_path(self.destination)
+            else:
+                func(order)
+        return wrapper
+
+    @property
+    def current_task(self) -> Task:
+        if len(self.task_list) > 0:
+            return self.task_list[0]
 
     @property
     def destination(self) -> Location:
-        if self.current_action is not None:
-            return self.current_action.destination
+        if self.current_task is not None:
+            return self.current_task.destination
 
-    def _deliver(self, product: Product) -> NoReturn:
-        """ Contains the logic for delivery,
-        runs each turn while the drone is in delivery mode.
+    @_requires_location
+    def _deliver(self, order: Order):
+        """ Contains the logic for delivery, runs each turn while the drone is in delivery mode. """
+        for product_id in order.products:
+            self.inventory.remove(product_id)
 
-        :param product: The package to be delivered
-        """
-        if self.location == self.destination:
-            self.current_action = None
-        else:
-            self.location = self.location.get_next_location_in_path(self.destination)
+        self.task_list.pop()
 
-    def _load(self, product: Product) -> NoReturn:
-        """ Contains the logic for loading,
-        runs each turn while the drone is in loading mode.
+    @_requires_location
+    def _load(self, warehouse: Warehouse, product_id: int, amount: int):
+        """ Contains the logic for loading, runs each turn while the drone is in loading mode. """
+        warehouse.get(product_id, amount)
 
-        :param product: The package to be loaded
-        """
-        if self.location == self.destination:
-            self.current_action = None
-        else:
-            self.location = self.location.get_next_location_in_path(self.destination)
+        for _ in range(amount):
+            self.inventory.append(product_id)
 
-    def _unload(self, product: Product) -> NoReturn:
-        """ Contains the logic for unloading,
-        runs each turn while the drone is in unloading mode.
+        self.task_list.pop()
 
-        :param product: The package to be unloaded
-        """
-        if self.location == self.destination:
-            self.current_action = None
-        else:
-            self.location = self.location.get_next_location_in_path(self.destination)
+    @_requires_location
+    def _unload(self, warehouse: Warehouse, product_id: int, amount: int):
+        """ Contains the logic for unloading, runs each turn while the drone is in unloading mode. """
+        for _ in range(amount):
+            self.inventory.remove(product_id)
 
-    def _drone_action_logic(self, func: Callable, destination: Location, package: Product, dry: bool = False):
-        """ Commands the drone to deliver the package to the destination.
+        warehouse.put(product_id, amount)
 
-        :param func: The function that will run while the drone is doing the action.
-        :param destination: The delivery destination.
-        :param package: The package to be delivered.
-        :param dry: If True, the drone wont do the action,
-            but the function will still return the duration.
+        self.task_list.pop()
 
-        :return: The amount of turns required for the drone to complete the action.
-        """
-        if dry is False:
-            self.current_action = ActionTuple(func, destination, package)
+    def deliver(self, order: Order):
+        """ Order the drone to deliver an order """
+        task: Task = Task(self._load, order.location, (order, ))
 
-        return self.location.distance(destination)
+        self.task_list.append(task)
 
-    def deliver(self, destination: Location, package: Product, dry: bool = False) -> int:
-        """ Commands the drone to deliver the package to the destination.
-        Uses the '_drone_action_logic' to set the drone's goal and return duration.
-        """
-        return self._drone_action_logic(self._deliver, destination, package, dry)
+    def load(self, warehouse: Warehouse, product_id: int, amount: int):
+        """ Order the drone to load a certain amount of product """
+        task: Task = Task(self._load, warehouse.location, (warehouse, product_id, amount))
 
-    def load(self, destination: Location, package: Product, dry: bool = False) -> int:
-        """ Commands the drone to load the package to the destination.
-        Uses the '_drone_action_logic' to set the drone's goal and return duration.
-        """
-        return self._drone_action_logic(self._load, destination, package, dry)
+        self.task_list.append(task)
 
-    def unload(self, destination: Location, package: Product, dry: bool = False) -> int:
-        """ Commands the drone to unload the package to the destination.
-        Uses the '_drone_action_logic' to set the drone's goal and return duration.
-        """
-        return self._drone_action_logic(self._unload, destination, package, dry)
+    def unload(self, warehouse: Warehouse, product_id: int, amount: int):
+        """ Order the drone to unload a certain amount of product """
+        task: Task = Task(self._unload, warehouse.location, (warehouse, product_id, amount))
+
+        self.task_list.append(task)
 
     def turns_left(self) -> int:
         """ Amount of turns until the drone finishes his current action. """
-        return self.location.distance(self.current_action.destination) + 1
+        return self.location.distance(self.current_task.destination) + 1
 
     def do_turn(self):
         """ Need to be called every turn,
         causes the drone the do his current action
         """
-        action = self.current_action
+        action = self.current_task
 
         if action is not None:
-            action.func(package=action.package, *action.args, **action.kwargs)
+            action.func(*action.args)
--- objects/product.py
+++
@@ -1,7 +0,0 @@
-from typing import NamedTuple
-
-
-class Product(NamedTuple):
-    """ Describes a package object """
-    package_id: int
-    weight: int
